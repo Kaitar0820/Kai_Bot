fec2cd44b3c00327aa270a43bdcf419febb1cc31
Kaitar0820 <53996097+Kaitar0820@users.noreply.github.com>
2020-01-30 19:45:28 +0800

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,13 +1,27 @@
 import discord
-from discord.ext import commands
 import json
+import random
+
+from discord.ext import commands
 
 with open('setting.json', 'r', encoding='utf8') as jfile:
     jdata = json.load(jfile)
 
 
 
-bot = commands.Bot(command_prefix='[')
+bot = commands.Bot(command_prefix='!')
+
+
+@bot.command()
+async def 傻眼(ctx):
+    random_pic = random.choice(jdata["pic"])
+    pic = discord.File(random_pic)
+    await ctx.send(file = pic)
+
+@bot.command()
+async def 尬(ctx):
+    random_pic = random.choice(jdata["url_pic"])
+    await ctx.send(random_pic)
 
 @bot.event
 async def on_ready():
@@ -26,7 +40,6 @@ async def on_member_remove(member):
 @bot.command()
 async def ping(ctx):
     await ctx.send(f"{round(bot.latency*1000)}(ms)")
- 
-   
 
-bot.run(jdata['TOKEN'])
+
+bot.run('NjcyMDAzODEzNzk1NjkyNTU0.XjIwTQ.Reggba0_RyYioJVCiyW3EvKTuxU')
